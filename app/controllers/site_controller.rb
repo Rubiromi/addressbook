14d0240b30faf80bfacde902901d39b5e38edabc
Rubiromi <hiromi.deyoung@gmail.com>
2015-01-13 18:37:03 -0700

    Set up devise. Created User model.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,2 +1,9 @@
 class SiteController < ApplicationController
+
+  def index
+    @title = "Home"
+    users = User.all
+    # @users = users_in_range(users, 7).compact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root 'site#index'
+  devise_for :users
+  root "site#index"
 end
