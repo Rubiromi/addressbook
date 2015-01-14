a70e0079061049a560cc39b377031606a942202f
Rubiromi <hiromi.deyoung@gmail.com>
2015-01-14 08:46:08 -0700

    Added an address model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :last_name, :phone, :zip, :street1, :street2, :city, :state, :country, presence: true
+
 end
