7e733a15db54b3bbf5263a087864ce3cb35242dc
Rubiromi <hiromi.deyoung@gmail.com>
2015-01-14 20:56:47 -0700

    Added addresses controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'addresses/index'
+
+  get 'addresses/show'
+
+  get 'addresses/new'
+
+  get 'addresses/create'
+
+  get 'addresses/edit'
+
+  get 'addresses/update'
+
+  get 'addresses/destroy'
+
   devise_for :users
 
   resources :site, only: [:index]
